d461a99333cc55a6bc096c7fdcea46cb9b2c41ec
James Kim <jhlink@users.noreply.github.com>
2018-11-05 19:47:05 -0500

    task[2]: Create rule providing functional testing of single activity

--- app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderActivityBasicTest.java
@@ -16,6 +16,7 @@
 
 package com.example.android.teatime;
 
+import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 /**
@@ -27,7 +28,9 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class OrderActivityBasicTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    // DONE (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<OrderActivity> mActivityTestRule = new ActivityTestRule<>(OrderActivity.class);
 
     // TODO (3) Finish writing this test which will:
     //          - Check that the initial quantity is zero
